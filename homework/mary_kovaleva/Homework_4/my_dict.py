f4dd47747a32754809fea0980510a1771a0f89ce
MikageH24 <mary.kovaleva1999@gmail.com>
2025-01-05 14:25:42 +0300

    Changed the type of the key

--- homework/mary_kovaleva/Homework_4/my_dict.py
+++ homework/mary_kovaleva/Homework_4/my_dict.py
@@ -1,12 +1,12 @@
 my_dict = {'tuple': (1, 2, 3.14, 'text', False), 'list': [1, 2.5, 'text', [1, 2, 3], True], 'dict': {1: 'one', 2: 'two',
-           3: 'three', 4: 'four', 'five': (78, 54, 12, 0, 78)}, 'set': {1, 0.5, 123, (1, ), 'text'}}
+           3: 'three', 4: 'four', ('five', ): (78, 54, 12, 0, 78)}, 'set': {1, 0.5, 123, (1, ), 'text'}}
 
 print(my_dict['tuple'][-1])
 
 my_dict['list'].append('last')
 my_dict['list'].pop(1)
 
-my_dict['dict']['i am a tuple'] = (1, 2, 3, 4, 5)
+my_dict['dict'][('i am a tuple', )] = (1, 2, 3, 4, 5)
 my_dict['dict'].pop(1)
 
 my_dict['set'].add(432)
